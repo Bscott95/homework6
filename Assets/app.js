ab8c9dd5aa841445f4e52e9df11f54d92b023567
Brandon Scott <btfs95@gmail.com>
2020-08-16 20:06:23 -0400

    basically final

--- Assets/app.js
+++ Assets/app.js
@@ -1,32 +1,18 @@
 $(document).ready(function () {
-
-    let location = "Boston"
+    // variables
+    // Boston is the default location
+    let location = "Boston" 
     const APIKey = "78d4820b52a05a5e039fd595437c5ac0";
     let cityCount = 0
+    let citiesIncluded = []
 
-    // updates current time locally
-    // $('#currentDate').text(moment().format('LLL'))
-    // let Date = moment().format('LLL')
-
-    $(document).on("click", "#submitEl", function (event) {
-        event.preventDefault();
-        console.log('run')
-        let loc = $('#locVal').val()
-        console.log()
-        console.log(loc)
-        latLongPull(loc)
-    });
-
-    // $('#submitEl').on('click', function(event){
-    //     event.preventDefault();
-    //     console.log('clicked')
-    // })
-
+    // takes the location requested and runs an ajax request pulling the lat and long of the city
+    // the runs the weatherSearch function taking in the lat and long 
+    // updates the current city ID on screen
+    // checks if the city has been searched before, and if not, runs the appendSearch function
     function latLongPull(location) {
         // Here we are building the URL we need to query the database
         let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}`;
-        // We then created an AJAX call
-        console.log(queryURL)
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -36,9 +22,12 @@ $(document).ready(function () {
             let long = response.coord.lon
             weatherSearch(lat,long)  
         });
-        appendSearch(location)
+        if (citiesIncluded.includes(location.toLowerCase()) === false){
+            appendSearch(location)
+        }
     }
 
+    // Takes in the lat and long and pulls most of the relavent data we need to update
     function weatherSearch(lat, long) {
         // Here we are building the URL we need to query the database
         let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${APIKey}`
@@ -47,23 +36,48 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            let fa = ((response.current.temp - 273.15) * 1.80 + 32).toFixed(2);
-            console.log(fa);
-            $('#currentTemp').text(fa + " F");
+            // updates current info section with relevent data
+            let curTemp = tempConversion(response.current.temp)
+            $('#currentTemp').text(curTemp + " F");
+            // converts windspeed from m/s to mph
             let wsmph = (response.current.wind_speed * 2.237).toFixed(2);
             $('#currentWindSpeed').text(wsmph + "mph")
             $('#currentHumidity').text(response.current.humidity + " %")
             $('#currentUVIndex').text(response.current.uvi)  
+            // retrieves the current and timezone offset value in Unix UTC 
+            // use the Date object and toUTCString method to convert the time
+            // multiply by 1000 because Date works in miliseconds.
+            // remove the last three characters of the string
             linuxUTC = response.current.dt
             timeZone = response.timezone_offset
-            console.log(new Date(linuxUTC*1000))
-            console.log(new Date((linuxUTC*1000)+(timeZone*1000)))
-            $('#currentDate').text(response.current.dt)
+            displayTime = new Date((linuxUTC*1000)+(timeZone*1000)).toUTCString().slice(0, -3)
+            $('#currentDate').text(displayTime)
+            let iconVal = response.current.weather[0].icon
+            $('#currentIcon').attr('src', `http://openweathermap.org/img/wn/${iconVal}.png`)
+        
+            // updates the forecase section with relevent data using a forloop. same logic as above
+            for(let i=1; i<=5;i++){
+                let forTemp = tempConversion(response.daily[i].temp.day)
+                $(`#${i}-temp`).text(forTemp + " F");
+                $(`#${i}-humid`).text(response.daily[i].humidity + " %")
+                linuxUTC = response.daily[i].dt
+                forDisplayTime = new Date((linuxUTC*1000)+(timeZone*1000)).toDateString()
+                $(`#${i}-date`).text(forDisplayTime)
+                forIconVal = response.daily[i].weather[0].icon
+                $(`#${i}-icon`).attr('src', `http://openweathermap.org/img/wn/${forIconVal}.png`)
+            }
         });
     }
 
-    latLongPull(location)
+     // converts the temp from kelvin to F. Takes in a kelvin value and returns the converted value 
+     function tempConversion(temp){
+        let convertedTemp = ((temp - 273.15) * 1.80 + 32).toFixed(2);
+        return convertedTemp
+    }
 
+    // appends searched cities to the left aside. Takes in a location variable of the city searched. 
+    // limits the number of cities that can be added and searched by updating and checking variable cityCount
+    // pushes the location to the array citiesIncluded
     function appendSearch(location){
         if(cityCount > 7){
             alert("Can only hold 8 cities. Please refresh the paage to add new ones")
@@ -73,17 +87,24 @@ $(document).ready(function () {
         $city.attr('id',`${location}`)
         $('#savedCities').append($city)
         cityCount++
+        citiesIncluded.push(location.toLowerCase())
         }
     }
 
-    $(`.list-group-item`).on('click', function(event){
-        console.log(event)
-        latLongPull()
+    // on submitting a search, takes the input of a city and runs the latLongPull function, 
+    // passing along the city or value of the input
+    $(document).on("click", "#submitEl", function (event) {
+        event.preventDefault();
+        let loc = $('#locVal').val()
+        latLongPull(loc)
+        $('#locVal').text('')
+    }); 
+
+    // when clicking on a previously searched city, pulls cities weather information back up
+    $(document).on('click', `.list-group-item`, function(event){
+        latLongPull(event.target.id)
     })
 
-
-
-
-
-
+    // function call with the default location - currently Boston
+    latLongPull(location)
 })
